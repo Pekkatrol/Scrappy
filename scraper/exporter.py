f9084cc5cde6e7795abb8eca9b4009c6278ada96
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-02-21 00:57:16 +0100

    [+] Add the export to csv function.

--- scraper/exporter.py
+++ scraper/exporter.py
@@ -1,2 +1,8 @@
-def export_to_csv():
-    return
+import pandas as pd
+
+def export_to_csv(data, filename) -> None:
+    """
+    This function take a dictionnary and a filename, transform the dictionnary into a csv file with the filename.
+    """
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
