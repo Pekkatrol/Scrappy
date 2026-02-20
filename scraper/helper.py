e1c2ffd4224b89ec81990fcade502157db4510d1
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-02-21 00:33:03 +0100

    [+] Add the print help function to show how to use the program.

---
+++ scraper/helper.py
@@ -0,0 +1,11 @@
+def print_help() -> None:
+    """
+    Print the help for the command to execute the main function correctly.
+    """
+    print("Right way to execute: ")
+    print("    main.py [URL] [OUTPUT_CSV_NAME]")
+    print("\nIf you need help try:")
+    print("    main.py -h")
+    print("\nArguments :")
+    print("    URL : an url of a site.")
+    print("    OUTPUT_CSV_NAME : the name of the output file in the data folder.")
